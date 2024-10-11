345a2df9e69b9c034388014ccb25482fca9bbf7c
Gerdinebehr <gerdine672@revature.net>
2024-10-11 00:57:08 -0400

    login page, rudimentary register page - desperately in need of some CSS

--- frontend/src/Pages/RegisterPage/RegisterPage.tsx
+++ frontend/src/Pages/RegisterPage/RegisterPage.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./RegisterPage.css";
 
 function RegisterPage() {
-  return <div>RegisterPage</div>;
+  return (
+    <div>
+      <h2>Register for an account</h2>
+      <input type="email" placeholder="Email" />
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      <button>Register</button>
+    </div>
+  );
 }
+ 
 
 export default RegisterPage;
