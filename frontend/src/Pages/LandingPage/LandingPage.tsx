22058cd23c7214c596f86414a335a3274bae0a96
Jason Weaver <jason462@revature.net>
2024-10-14 12:21:30 -0400

    reformatted bg and internal organization

--- frontend/src/Pages/LandingPage/LandingPage.tsx
+++ frontend/src/Pages/LandingPage/LandingPage.tsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
-import Post from "../../Components/Post/Post"
+import Post from "../../Components/Post/Post";
 import axios from "axios";
+import "./LandingPage.css";
 // import "../../index.css";
 
 // // Simulate getting posts from a backend
 const getPosts = async () => {
-  return (
-    { title: "Post 1", content: "This is the first post's content." }
-  );
+  return { title: "Post 1", content: "This is the first post's content." };
 };
 
 function LandingPage() {
@@ -17,74 +16,85 @@ function LandingPage() {
 
   const handleLoadMore = () => {
     setPage(page + 1);
-  }
+  };
 
-/**
- * INTERCEPTORS
- */
-axios.interceptors.request.use((config: any): any => {
-  return config
-}, (error) => {
-  return Promise.reject(error);
-});
+  /**
+   * INTERCEPTORS
+   */
+  // Request Interceptor
+  axios.interceptors.request.use(
+    (config: any): any => {
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
 
-// Response Interceptor
-axios.interceptors.response.use((response: any): any => {
-  return response;
-}, (error) => {
-  console.error(error);
-  return Promise.reject(error);
-})
+  // Response Interceptor
+  axios.interceptors.response.use(
+    (response: any): any => {
+      return response;
+    },
+    (error) => {
+      console.error(error);
+      return Promise.reject(error);
+    }
+  );
 
-/**
- * INITIAL LANDING
- */
-useEffect(() => {
-  axios.get(`http://localhost:4000/api/forums/landing?page=${page}`)
-    .then((response) => {
-      // Axios will automatically turn the response into a JS object
-      setPosts(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-}, [])
+  const getPosts = async () => {
+    await axios
+      .get(`http://localhost:4000/api/forums/landing?page=${page}`)
+      .then((response) => {
+        // Axios will automatically turn the response into a JS object
+        setPosts(response.data[0]);
 
-/**
- * LOAD MORE
- */
-useEffect(() => {
-    axios.get(`http://localhost:4000/api/forums/landing?page=${page}`)
-    .then((response) => {
-      // Axios will automatically turn the response into a JS object
-      setPosts(response.data[0]);
+        if (response.data[1] <= 4 + (page - 1) * 4) {
+          setIsClickable(false);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-      if (response.data[1] <= (4 + ((page-1) * 4))) {
-        setIsClickable(false);
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-}, [page])
+  /**
+   * INITIAL LANDING
+   */
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  /**
+   * LOAD MORE
+   */
+  useEffect(() => {
+    getPosts();
+  }, [page]);
 
   return (
     <div className="landing-page">
-      <div className="post-list">
-         {posts.map((post: any, ind: number) => (
-           <Post key={ind} title={post.title} username={post.written_by} post_id={post.post_id} />
+      <div>
+        {posts.map((post: any, ind: number) => (
+          <Post
+            key={ind}
+            title={post.title}
+            username={post.written_by}
+            post_id={post.post_id}
+            content={post.body}
+          />
         ))}
-       </div>
-        <button
-          type="button" 
-          className="btn btn-outline-secondary load-button-format"
-          onClick={() => handleLoadMore()}
-          disabled={!isClickable}
-        >
-        Load more
-        </button>
+      </div>
+      <button
+        type="button"
+        className="btn btn-outline-secondary load-button-format"
+        onClick={() => handleLoadMore()}
+        disabled={!isClickable}
+      >
+        {isClickable ? "Load More" : "End of Page"}
+      </button>
     </div>
-  )
-};
+  );
+}
 
 export default LandingPage;
--- frontend/src/Pages/PostPage/PostPage.tsx
+++ frontend/src/Pages/PostPage/PostPage.tsx
@@ -3,20 +3,20 @@ import "./PostPage.css";
 import { useLocation, useParams } from "react-router-dom";
 
 function PostPage() {
-  const {post_id} = useParams();
+  const { post_id } = useParams();
   const location = useLocation();
 
   const title = location.state?.title;
   const username = location.state?.username;
+  const content = location.state?.content;
 
-  return <div>
-    <h3>
-      {title}
-    </h3>
-    <p>
-      {username}
-    </p>
-  </div>;
+  return (
+    <div className="post-page">
+      <h3 className="text-format">{title}</h3>
+      <p className="text-format">{username}</p>
+      <p className="text-format">{content}</p>
+    </div>
+  );
 }
 
 export default PostPage;
