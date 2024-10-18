f350e7154915cfd069d0ccea27372fdd04fe4183
Aaron Cordeau <akcordeau@gmail.com>
2024-10-18 12:18:32 -0400

    Update encounterPage formatting

--- frontend/src/Components/RosterCard/RosterCard.tsx
+++ frontend/src/Components/RosterCard/RosterCard.tsx
@@ -9,11 +9,14 @@ interface RosterCardProps {
 
 const RosterCard: React.FC<RosterCardProps> = ({ name, page, onRemove }) => {
   return (
-    <div className="roster-card d-flex justify-content-between align-baseline m-1 mx-2">
+    <div className="roster-card d-flex justify-content-between align-items-baseline m-1 mx-2">
       <a href={page} target="_blank" rel="noopener noreferrer">
         {name}
       </a>
-      <button className="monster-remove-button" onClick={onRemove}>
+      <button
+        className="monster-remove-button d-flex align-items-center align-content-center"
+        onClick={onRemove}
+      >
         X
       </button>
     </div>
