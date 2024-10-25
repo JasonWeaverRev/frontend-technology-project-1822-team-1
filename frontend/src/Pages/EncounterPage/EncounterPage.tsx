3e1075d457586eecb81ee411d960efa38408504e
Jason Weaver <jason462@revature.net>
2024-10-25 11:01:51 -0400

    landing-posts merge

--- frontend/src/Pages/EncounterPage/EncounterPage.tsx
+++ frontend/src/Pages/EncounterPage/EncounterPage.tsx
@@ -43,7 +43,7 @@ function EncounterPage() {
   // #endregion
 
   const saveEncounter = async () => {
-    try {
+    try {3.81.216.218
       const response = await axios.post(
         `http://3.81.216.218:4000/api/encounters/encounter`,
         {
