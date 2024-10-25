56e474d8091a8b9fb897d54639742432c491ac5f
Aaron Cordeau <akcordeau@gmail.com>
2024-10-25 12:06:15 -0400

    remove extra function

--- frontend/src/Pages/LoginPage/LoginPage.tsx
+++ frontend/src/Pages/LoginPage/LoginPage.tsx
@@ -3,26 +3,6 @@ import "./LoginPage.css";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const storeLoggedInUser = async () => {
-  try {
-    const response = await axios.get(
-      "http://3.81.216.218:4000/api/accounts/profile",
-      {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      }
-    );
-
-    console.log(response);
-
-    localStorage.setItem("username", response.data.userProfile.username);
-    localStorage.setItem("role", response.data.userProfile.role);
-  } catch (error) {
-    console.error("Error fetching user profile: ", error);
-  }
-};
-
 function LoginPage() {
   const [identifier, setIdentifier] = useState<string>("");
   const [password, setPassword] = useState<string>("");
