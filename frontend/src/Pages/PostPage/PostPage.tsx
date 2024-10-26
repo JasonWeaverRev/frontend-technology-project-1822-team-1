eda21de97303ef1a1af5dbbe6ef37237f83bf633
Jason Weaver <jason462@revature.net>
2024-10-26 13:58:56 -0400

    changed from local to IP

--- frontend/src/Pages/PostPage/PostPage.tsx
+++ frontend/src/Pages/PostPage/PostPage.tsx
@@ -79,7 +79,7 @@ function PostPage() {
   const getComments = async () => {
     await axios
       .get(
-        `http://localhost:4000/api/forums/comments/post?id=${postId}&page=${page}`
+        `http://3.81.216.218:4000/api/forums/comments/post?id=${postId}&page=${page}`
       )
       .then((response) => {
         setComments(response.data[0]);
@@ -135,7 +135,7 @@ function PostPage() {
   const handleUpvote = async () => {
     try {
       const response = await axios.post(
-        `http://localhost:4000/api/forums/like`,
+        `http://3.81.216.218:4000/api/forums/like`,
         {
           post_id: postId,
         }
@@ -153,7 +153,7 @@ function PostPage() {
   const handleDownvote = async () => {
     try {
       const response = await axios.post(
-        `http://localhost:4000/api/forums/dislike`,
+        `http://3.81.216.218:4000/api/forums/dislike`,
         {
           post_id: postId,
         }
@@ -170,7 +170,7 @@ function PostPage() {
    */
   const getLikes = async () => {
     await axios
-      .get(`http://localhost:4000/api/forums/posts/likes/${postId}`)
+      .get(`http://3.81.216.218:4000/api/forums/posts/likes/${postId}`)
       .then((response) => {
         setLikes(response.data);
       })
@@ -185,7 +185,7 @@ function PostPage() {
   const getLikedBy = async () => {
     try {
       const response = await axios.get(
-        `http://localhost:4000/api/forums/posts/${postId}`
+        `http://3.81.216.218:4000/api/forums/posts/${postId}`
       );
 
       setLikedByList(response.data.liked_by);
@@ -223,7 +223,7 @@ function PostPage() {
   const fetchReplies = async (parentId: string | undefined): Promise<any[]> => {
     try {
       const response = await axios.get(
-        `http://localhost:4000/api/forums/comments/post?id=${parentId}&page=1`
+        `http://3.81.216.218:4000/api/forums/comments/post?id=${parentId}&page=1`
       );
 
       const replies = response.data[0];
@@ -259,7 +259,7 @@ function PostPage() {
 
     try {
       const response = await axios.post(
-        `http://localhost:4000/api/forums/${responseId}`,
+        `http://3.81.216.218:4000/api/forums/${responseId}`,
         {
           body: commentText,
         }
@@ -312,7 +312,7 @@ function PostPage() {
 
     try {
       const response = await axios.get(
-        `http://localhost:4000/api/encounters/encounter?encounter_id=${encounterId}`
+        `http://3.81.216.218:4000/api/encounters/encounter?encounter_id=${encounterId}`
       );
 
       setEncounterPost(response.data.encounter);
