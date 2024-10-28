00ffb45b1fa61d7d1dbc6f4e37ca7b88f026a362
rickyly1 <lyr5@uci.edu>
2024-10-27 17:05:48 -0700

    cypress tests for profile / campaign pages

--- frontend/cypress/e2e/profile/profilePageSpec.cy.js
+++ frontend/cypress/e2e/profile/profilePageSpec.cy.js
@@ -1,4 +1,4 @@
-describe("/encounter-creation logged in", () => {
+describe("/encounter-creation", () => {
   beforeEach(() => {
     cy.visit(
       "http://1822-team-1-website-production.s3-website-us-east-1.amazonaws.com"
@@ -32,5 +32,41 @@ describe("/encounter-creation logged in", () => {
     cy.get('#navbarDropdown').click();
     cy.get(':nth-child(1) > .dropdown-item').click();
     cy.get(':nth-child(1) > #encounter-button-container > span > :nth-child(1)').click();
+    cy.get('.confirm-delete-btn').should("exist");
+    cy.get('.cancel-btn').should("exist");
+  });
+
+  it("Assign Campaign to Encounter", () => {
+    cy.get('#navbarDropdown').click();
+    cy.get(':nth-child(1) > .dropdown-item').click();
+    cy.get(':nth-child(1) > #encounter-button-container > span > .pencil').click();
+    cy.get('.confirm-delete-btn').should("exist");
+    cy.get('.cancel-btn').should("exist");
+  });
+
+  it("Navigates to Campaign", () => {
+    cy.get('#navbarDropdown').click();
+    cy.get(':nth-child(1) > .dropdown-item').click();
+    cy.get('[href="/profile/rickytest/testing%20text%20field%20clear"]').click();
+    cy.get('.content-card').should("exist");
+  });
+
+  it("Navigates to Campaign + remove button exists", () => {
+    cy.get('#navbarDropdown').click();
+    cy.get(':nth-child(1) > .dropdown-item').click();
+    cy.get('[href="/profile/rickytest/testing%20text%20field%20clear"]').click();
+    cy.get('.delete-button').should("exist");
+  });
+
+  it("Navigates to Encounter", () => {
+    cy.get('#navbarDropdown').click();
+    cy.get(':nth-child(1) > .dropdown-item').click();
+    cy.get(':nth-child(1) > .title-link > h3').click();
+  });
+
+  it("Navigates to Forum Post", () => {
+    cy.get('#navbarDropdown').click();
+    cy.get(':nth-child(1) > .dropdown-item').click();
+    cy.get('a > .content-card > h3').click();
   });
 })
